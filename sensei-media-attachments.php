fb1668dd9d0ec61ff5b83c14413f98914b4ab2e6
Hugh Lashbrooke <hugh@woothemes.com>
2014-01-15 10:48:48 +0200

    Adding woo-includes and updater functions

--- sensei-media-attachments.php
+++ sensei-media-attachments.php
@@ -3,7 +3,7 @@
  * Plugin Name: Sensei Media Attachments
  * Version: 1.0.0
  * Plugin URI: http://www.woothemes.com/
- * Description: Attach media files to lessons and courses in Sensei
+ * Description: Enhance your lessons by attaching media files to lessons and courses in Sensei
  * Author: WooThemes
  * Author URI: http://www.woothemes.com/
  * Requires at least: 3.5
@@ -16,7 +16,37 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-require_once( 'classes/class-sensei-media-attachments.php' );
+/**
+ * Required functions
+ */
+if ( ! function_exists( 'woothemes_queue_update' ) ) {
+	require_once( 'woo-includes/woo-functions.php' );
+}
 
-global $sensei_media_attachments;
-$sensei_media_attachments = new Sensei_Media_Attachments( __FILE__ );
+/**
+ * Plugin updates
+ */
+woothemes_queue_update( plugin_basename( __FILE__ ), '788647a9a1d8ef5c95371f0e69223a0f', '290551' );
+
+/**
+ * Functions used by plugins
+ */
+if ( ! class_exists( 'WooThemes_Sensei_Dependencies' ) ) {
+	require_once 'woo-includes/class-woothemes-sensei-dependencies.php';
+}
+
+/**
+ * Sensei Detection
+ */
+if ( ! function_exists( 'is_sensei_active' ) ) {
+  function is_sensei_active() {
+    return WooThemes_Sensei_Dependencies::sensei_active_check();
+  }
+}
+
+if( is_sensei_active() ) {
+	require_once( 'classes/class-sensei-media-attachments.php' );
+
+	global $sensei_media_attachments;
+	$sensei_media_attachments = new Sensei_Media_Attachments( __FILE__ );
+}
