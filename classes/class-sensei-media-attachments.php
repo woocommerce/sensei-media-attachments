13b5d06b7d6fb591dbac362d7986586c577f69c7
Dan Johnson <dan.johnson@automattic.com>
2016-04-08 15:15:04 +0100

    Fix location of Course media output. Closes #6

--- classes/class-sensei-media-attachments.php
+++ classes/class-sensei-media-attachments.php
@@ -28,7 +28,7 @@ class Sensei_Media_Attachments {
 		add_action( 'save_post', array( $this, 'save_media_meta_box' ) );
 
 		// Media files display
-		add_action( 'sensei_course_single_lessons', array( $this, 'display_attached_media' ), 9 );
+		add_action( 'sensei_single_course_content_inside_before', array( $this, 'display_attached_media' ), 35 );
 		add_action( 'sensei_lesson_single_meta', array( $this, 'display_attached_media' ), 1 );
 	}
 
