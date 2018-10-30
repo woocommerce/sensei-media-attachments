7cd5797e169c9700b52f05beac9a7fbb41fd610f
Alex Sanford <alex.sanford1@gmail.com>
2018-10-30 14:59:43 -0300

    Add gulp package tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
 var gulp      = require( 'gulp' );
 var wpPot     = require( 'gulp-wp-pot' );
 var sort      = require( 'gulp-sort' );
+var zip       = require( 'gulp-zip' );
+
+var paths = {
+	packageContents: [
+		'assets/**/*',
+		'changelog.txt',
+		'LICENSE',
+		'classes/**/*',
+		'woo-includes/**/*',
+		'lang/**/*',
+		'README.md',
+		'sensei-media-attachments.php',
+	],
+	packageDir: 'build/sensei-media-attachments',
+	packageZip: 'build/sensei-media-attachments.zip'
+};
 
 gulp.task( 'pot', function() {
         return gulp.src( [ '**/**.php', '!node_modules/**'] )
@@ -10,3 +26,22 @@ gulp.task( 'pot', function() {
                 }) )
                 .pipe( gulp.dest( 'lang' ) );
 });
+
+gulp.task( 'clean', gulp.series( function( cb ) {
+	return del( [
+		'build'
+	], cb );
+} ) );
+
+gulp.task( 'copy-package', function() {
+	return gulp.src( paths.packageContents, { base: '.' } )
+		.pipe( gulp.dest( paths.packageDir ) );
+} );
+
+gulp.task( 'zip-package', function() {
+	return gulp.src( paths.packageDir + '/**/*', { base: paths.packageDir + '/..' } )
+		.pipe( zip( paths.packageZip ) )
+		.pipe( gulp.dest( '.' ) );
+} );
+
+gulp.task( 'package', gulp.series( 'copy-package', 'zip-package' ) );
