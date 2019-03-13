f6550c463f53edd38f346f33b5f2ad660b6cf3c3
Jake Oehler Morrison <jake.morrison@automattic.com>
2019-03-13 16:00:38 +0000

    Update language in dependency notice

--- classes/class-sensei-media-attachments-dependency-checker.php
+++ classes/class-sensei-media-attachments-dependency-checker.php
@@ -138,7 +138,7 @@ class Sensei_Media_Attachments_Dependency_Checker {
 		}
 
 		// translators: %1$s is the minimum version number of Sensei that is required.
-		$message = sprintf( __( '<strong>Sensei Media Attachments</strong> requires the plugin <strong>Sensei</strong> (minimum version: <strong>%1$s</strong>) is installed and activated.', 'sensei_media_attachments' ), self::MINIMUM_SENSEI_VERSION );
+		$message = sprintf( __( '<strong>Sensei Media Attachments</strong> requires that the plugin <strong>Sensei</strong> (minimum version: <strong>%1$s</strong>) is installed and activated.', 'sensei_media_attachments' ), self::MINIMUM_SENSEI_VERSION );
 		echo '<div class="error"><p>';
 		echo wp_kses( $message, array( 'strong' => array() ) );
 		echo '</p></div>';
