ee6548820eba03a29a17bdd4bab05832340276f2
Jake Oehler Morrison <jake.morrison@automattic.com>
2019-03-20 09:44:27 +0000

    Add post type to filter

--- classes/class-sensei-media-attachments.php
+++ classes/class-sensei-media-attachments.php
@@ -231,8 +231,9 @@ class Sensei_Media_Attachments {
 		 *
 		 * @param string  $media_heading Heading text for course or lesson page's attachments.
 		 * @param int     $post_id       Current post ID.
+		 * @param string  $post_type     Post type for current post.
 		 */
-		$media_heading = apply_filters( 'sensei_media_attachments_media_heading', $media_heading, $post->ID );
+		$media_heading = apply_filters( 'sensei_media_attachments_media_heading', $media_heading, $post->ID, $post_type );
 
 		$html = '<div id="attached-media">';
 		$html .= '<h2>' . esc_html( $media_heading ) . '</h2>';
