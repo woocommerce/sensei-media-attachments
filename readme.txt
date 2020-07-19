115c62a7d66c2fce4079fef81b4c4f19f0d33b4f
Donna Peplinskie <donnapep@gmail.com>
2020-07-19 15:56:24 -0400

    Bump minimum required WordPress version to 5.0

--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 === Sensei LMS Media Attachments ===
 Contributors: automattic, alexsanford1, donnapep, jakeom
 Tags: sensei lms, attachments, media, downloads
-Requires at least: 3.5
+Requires at least: 5.0
 Tested up to: 5.4
 Requires PHP: 5.6
 Stable tag: 2.0.2
--- sensei-media-attachments.php
+++ sensei-media-attachments.php
@@ -6,7 +6,7 @@
  * Description: Provide your students with easy access to additional learning materials, from audio files to slideshows and PDFs.
  * Author: Automattic
  * Author URI: https://automattic.com/
- * Requires at least: 3.5
+ * Requires at least: 5.0
  * Tested up to: 5.4
  * Requires PHP: 5.6
  *
